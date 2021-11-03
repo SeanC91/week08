91de84105dc8ff2bbbae58c7ead9412bc74fe90b
SeanC91 <91170815+SeanC91@users.noreply.github.com>
2021-11-03 11:55:21 +0800

    Add files via upload

---
+++ week8-6.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main( void )
+{
+   int i,j;
+   int n;
+
+   printf("請輸入大小:\n");
+   scanf("%d",&n);
+
+for(i=1;i<=n;i++){
+	for(j=1;j<=i;j++) printf(" ");
+	for(j=1;j<=n;j++) printf("*");
+	printf("\n");
+	}
+
+   return 0;
+}
